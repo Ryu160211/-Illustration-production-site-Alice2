7955be7b2141264ed266249cff417c3d4e0766a3
Ryu160211 <skebyongakaunto@gmail.com>
2023-12-24 10:17:32 +0900

    12月24日更新

---
+++ kobayashi/本番用/js/GALLERY2.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("clickableImage")
+  .addEventListener("click", function () {
+    alert("会員登録を完了させてください");
+  });
+
+function showAlert() {
+  alert("会員登録を完了させてください");
+}
