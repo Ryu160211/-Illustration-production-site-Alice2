94457754c16ac209a0531df7ff996f21dc50cb62
jeremi4h <fumiya.morikuni@gmail.com>
2024-01-11 09:32:11 +0900

    データベース用

--- 本番環境/init_database.py
+++ 本番環境/init_database.py
@@ -7,8 +7,7 @@ db_uri = "postgresql://postgres:postgres@localhost:5432/test"
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 db = SQLAlchemy(app)
 
-
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True)
     password = db.Column(db.String, unique=True)
@@ -52,6 +51,10 @@ class Follow(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     creator_id = db.Column(db.Integer, db.ForeignKey("creator.id"))
 
+class AdminUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True)
+    password = db.Column(db.String, unique=True)
 
 with app.app_context():
     db.create_all()
