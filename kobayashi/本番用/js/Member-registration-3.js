6ebf741595b27ab4b94ab5a3f5bfe0ba47f0875b
Ryu160211 <skebyongakaunto@gmail.com>
2024-01-15 12:22:37 +0900

    update

--- kobayashi/本番用/js/Member-registration-3.js
+++ kobayashi/本番用/js/Member-registration-3.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   // LocalStorageから値を取得
   var name = localStorage.getItem("name");
-  var Gmail = localStorage.getItem("Gmail");
   var PS = localStorage.getItem("PS");
 
   // 取得した値を表示
   document.getElementById("confirmedName").value = name;
-  document.getElementById("confirmedGmail").value = Gmail;
   document.getElementById("confirmedPS").value = PS;
 
   // 入力フィールドを読み取り専用に設定
   document.getElementById("confirmedName").readOnly = true;
-  document.getElementById("confirmedGmail").readOnly = true;
   document.getElementById("confirmedPS").readOnly = true;
 });
