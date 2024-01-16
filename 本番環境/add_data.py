b5f23f5ee251b43d62fd02d189292676a97d6f26
jeremi4h <fumiya.morikuni@gmail.com>
2024-01-16 10:13:08 +0900

    データ追加用

--- 本番環境/add_data.py
+++ 本番環境/add_data.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from init_database import db, db_uri, Character, Creator, Content
+from init_database import OtherWork, db, db_uri, Character, Creator, Content
 import pandas as pd
 
 app = Flask(__name__)
@@ -8,36 +8,44 @@ db.init_app(app)
 
 chara_df = pd.read_csv("./static/csv/character_data.csv")
 content_df = pd.read_csv("./static/csv/content_data.csv")
+other_df = pd.read_csv('./static/csv/otherwork_data.csv')
 creator_df = pd.read_csv("./static/csv/creator_data.csv")
 
 with app.app_context():
-    db.session.query(Character).delete()
-    db.session.query(Content).delete()
-    db.session.query(Creator).delete()
-    db.session.commit()
-    
     for index, row in chara_df.iterrows():
-        character = Character(id=row["id"], name=row["name"])
+        character = Character(id=row["id"], jp_name=row["jp_name"], en_name=row["en_name"])
         db.session.add(character)
 
     for index, row in content_df.iterrows():
         content = Content(
             id=row["id"],
             name=row["name"],
-            character_id=row["character_id"],
             filename=row["filename"],
             icon=row["icon"],
+            creator_id=row["creator_id"],
+            character_id=row["character_id"],
             desc=row["desc"],
         )
         db.session.add(content)
+
+    for index, row in other_df.iterrows():
+        other_work = OtherWork(
+            id=row["id"],
+            creator_id=row["creator_id"],
+            index=row["index"],
+            filename=row["filename"],
+            link=row["index"]
+        )
+        db.session.add(other_work)
 
     for index, row in creator_df.iterrows():
         creator = Creator(
             id=row["id"],
             name=row["name"],
             mail=row["mail"],
-            icon=row["icon"],
             introduce=row["introduce"],
+            icon=row["icon"],
+            background=row["background"],
             twitter=row["twitter"],
             pixiv=row["pixiv"],
             skeb=row["skeb"],
