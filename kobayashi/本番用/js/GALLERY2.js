d9866da6e067e35fef218fd8e630957ecbedfb52
Ryu160211 <skebyongakaunto@gmail.com>
2024-01-17 15:26:31 +0900

    1月17日更新

--- kobayashi/本番用/js/GALLERY2.js
+++ kobayashi/本番用/js/GALLERY2.js
@@ -7,3 +7,18 @@ document
 function showAlert() {
   alert("会員登録を完了させてください");
 }
+/*function changeImage() {
+  var imageElement = document.getElementById("heartImage");
+
+  // 画像の切り替え
+  if (imageElement.src.endsWith("ハートのマーク.png")) {
+    imageElement.src = "image/ハートのマーク2.png";
+  } else {
+    imageElement.src = "image/ハートのマーク.png";
+  }
+} */
+function handleFollowClick() {
+  // クリック時の処理をここに記述
+  alert("Follow button clicked!");
+  // 他の処理を追加する場合はここに追加
+}
