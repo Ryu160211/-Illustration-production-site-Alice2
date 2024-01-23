f7c5db0425dfe19dedcd47d303b788633e1d18bd
jeremi4h <fumiya.morikuni@gmail.com>
2024-01-23 15:08:25 +0900

    app

--- 本番環境/app.py
+++ 本番環境/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_login import LoginManager, login_user
 from init_database import OtherWork, db, db_uri, User, Creator, Content, Character, Like, Follow, AdminUser
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['FLASK_ADMIN_SWATCH'] = 'sandstone'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 db.init_app(app)
 
 class UserModelView(ModelView):
@@ -20,6 +25,10 @@ admin.add_view(UserModelView(OtherWork, db.session))
 admin.add_view(UserModelView(Content, db.session))
 admin.add_view(UserModelView(Character, db.session))
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
 def home():
     return redirect(url_for('contents'))
@@ -29,16 +38,18 @@ def login():
     if request.method == 'POST':
         name = request.form['name']
         password = request.form['password']
-        admin = AdminUser.query.filter_by(name=name).first()
-        if admin.password == password:
-            return redirect(url_for('admin.index'))
+        user = User.query.filter_by(name=name).first()
+        if check_password_hash(user.password, password):
+            login_user(user)
+            session['username'] = name
+            return redirect('contents')
     title = '東方立ち絵広場-ログイン'
     css = 'css/login.css'
     return render_template('login.html', title=title, css=css) 
 
 @app.route('/contents')
 def contents():
-    title = '東方立ち絵広場-gallery'
+    title = '東方立ち絵広場-contents'
     contents = Content.query.order_by(Content.id.asc()).all()
     return render_template('contents.html', css='css/contents.css', title=title, contents=contents)
 
@@ -68,8 +79,7 @@ def creator(creator_id):
 @app.route('/how')
 def how():
     title = '東方立ち絵広場-how to'
-    css = 'css/how.css'
-    return render_template('how.html', title=title, css=css)
+    return render_template('how.html', css='css/how.css', title=title)
 
 @app.route('/rule')
 def rule():
@@ -94,10 +104,16 @@ def signup3():
     title='東方立ち絵広場-会員登録'
     return render_template('signup3.html', title=title, css='css/signup3.css')
 
-@app.route('/signup4')
+@app.route('/signup4', methods=['GET', 'POST'])
 def signup4():
-    title='東方立ち絵広場-会員登録'
-    return render_template('signup4.html', title=title, css='css/signup4.css')
+    if request.method == 'POST':
+        name = request.form['name']
+        password = request.form['password']
+        user = User(name=name, password=generate_password_hash(password))
+        db.session.add(user)
+        db.session.commit()
+        title='東方立ち絵広場-会員登録'
+        return render_template('signup4.html', title=title, css='css/signup4.css')
 
 if __name__ == '__main__':
     app.run(port=123456, debug=True)
