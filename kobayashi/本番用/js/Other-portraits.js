ea6e6d8b2a0aa42fa3aec67ff0f700789f286d2b
Ryu160211 <skebyongakaunto@gmail.com>
2024-01-24 15:43:08 +0900

    1月24日更新

--- kobayashi/本番用/js/Other-portraits.js
+++ kobayashi/本番用/js/Other-portraits.js
@@ -1,12 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // triangle要素を取得
-    var triangle = document.querySelector('.triangle');
-    // Other-portraits要素を取得
-    var otherPortraits = document.querySelector('.Other-portraits');
-  
-    // triangleがクリックされた時の処理
-    triangle.addEventListener('click', function () {
-      // Other-portraitsにshowクラスを追加
-      otherPortraits.classList.add('show');
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // triangle要素を取得
+
+  // triangleがクリックされた時の処理
+  triangle.addEventListener("click", function () {
+    // Other-portraitsにshowクラスを追加
+    otherPortraits.classList.add("show");
   });
+});
