ab3753cda28443aa49280d3a0b1ed51aa1bf48a8
jeremi4h <fumiya.morikuni@gmail.com>
2024-01-24 16:12:04 +0900

    js

--- 本番環境/static/js/content.js
+++ 本番環境/static/js/content.js
@@ -1,13 +1,32 @@
-function changeImage() {
-  var imageElement = document.getElementById("heartImage");
-
-  // 画像の切り替え
-  if (imageElement.src.endsWith("ハートのマーク.png")) {
-    imageElement.src = "image/ハートのマーク2.png";
-  } else {
-    imageElement.src = "image/ハートのマーク.png";
+function changeImage(content_id) {
+  if (document.getElementById("like_button").dataset.is_login === "None") {
+    alert("ログインしてください");
+    return;
   }
+
+  var count_text = $("#like_count");
+  var image = $("#like-button");
+  $.ajax({
+    url: "/like",
+    type: "POST",
+    data: { content_id: content_id },
+  })
+    .done(function (received_data) {
+      var data = JSON.parse(received_data);
+      var is_like = data["is_like"];
+      var like_count = data["like_count"];
+      count_text.text(like_count);
+      if (is_like === "true") {
+        image.children("img").attr("src", "/static/image/common/unlike.png");
+      } else {
+        image.attr("src", "/static/image/common/like.png");
+      }
+    })
+    .fail(function () {
+      console.log("failed");
+    });
 }
+
 function handleFollowClick() {
   // クリック時の処理をここに記述
   alert("Follow button clicked!");
